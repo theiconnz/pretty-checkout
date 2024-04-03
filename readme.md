f12a53b6697c591e26bea5a9822bb0ed0e3f2d94
nuwinda <info@flow200.com>
2024-04-03 14:11:46 +1300

    first release

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Installation
+
+Install the package using composer
+
+composer require theiconnz/wholesale
+
+bin/magento setup:upgrade
+
+bin/magento setup:di:compile
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2023 Theiconnz. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Theiconnz_PrettyCheckout',
+    __DIR__
+);
